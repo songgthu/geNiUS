f13c1fbd349d56a86acc8f4caa9588b4d98cf46f
Song Thư Trịnh Hoài <songthu0711@gmail.com>
2023-05-28 22:10:28 +0700

    Milestone 1

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -17,7 +17,7 @@ function handleLogin(event) {
         password: password
       };
   
-    const baseUrl = window.location.hostname == '127.0.0.1' ? 'http://localhost:5501' : 'https://genius-gamma.vercel.app';
+    const baseUrl = window.location.hostname == '127.0.0.1' ? 'http://localhost:5501' : 'https://vercel.com';
     fetch(`${baseUrl}/login-user`, {
       method: 'POST',
       headers: {
