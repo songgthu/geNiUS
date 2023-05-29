b891d66b1de2a8f7af43727d49d507171282f712
songgthu <e1082977@u.nus.edu>
2023-05-29 22:53:20 +0700

    Test

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/html/login.html'));
