e09f59f88d75f6d272e7e495b0843d7a5b6d1683
songgthu <e1082977@u.nus.edu>
2023-06-02 19:13:41 +0700

    Update add task function

--- frontend/js/todo.js
+++ frontend/js/todo.js
@@ -1,108 +1,52 @@
-const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const create = document.querySelector('.addButton');
+var modal = document.getElementById("modal");
+create.addEventListener('click', createTask);
 
-  document.querySelector("#enter").addEventListener("click", () => {
-    const item = document.querySelector('#item');
-    createItem(item);
-  })
+var span = document.getElementsByClassName("close")[0];
+span.addEventListener('click', close);
 
-  document.querySelector("#item").addEventListener("keypress", (e) => {
-  if(e.key === "Enter"){
-    const item = document.querySelector("#item")
-    createItem(item)
+function createTask(){
+  modal.style.display="block";
+  const addQuery = document.querySelector('.saveTaskButton');
+  addQuery.addEventListener('click', modifyTask);
+}
+  // Function to add a new task
+  function modifyTask() {
+    // Get the input value
+    const taskInput = document.querySelector('.taskInput').value;
+    const deadline = document.querySelector('.deadlineInput').value;
+
+    // Create a new row
+    var table = document.querySelector('.task-table');
+    var row = table.insertRow();
+
+    // // Create cells for the task, deadline, and status
+    var taskCell = row.insertCell(0);
+    var deadlineCell = row.insertCell(1);
+    var statusCell = row.insertCell(2);
+
+    // // Set the values for the cells
+    taskCell.textContent = taskInput;
+    deadlineCell.textContent = deadline;
+
+    // // Create a checkbox and label for the status cell
+    var checkboxContainer = document.createElement('div');
+    checkboxContainer.className = 'checkbox-container';
+
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'statusCheckbox' + table.rows.length;
+    checkboxContainer.appendChild(checkbox);
+
+    var label = document.createElement('label');
+    label.htmlFor = 'statusCheckbox' + table.rows.length;
+    checkboxContainer.appendChild(label);
+
+    statusCell.appendChild(checkboxContainer);
+    close();
   }
-})
 
-function displayDate(){
-  let date = new Date()
-  date = date.toString().split(" ")
-  date = date[1] + " " + date[2] + " " + date[3] 
-  document.querySelector("#date").innerHTML = date 
-}
-
-function displayItems(){
-  let items = ""
-  for(let i = 0; i < itemsArray.length; i++){
-    items += `<div class="item">
-                <div class="input-controller">
-                  <textarea disabled>${itemsArray[i]}</textarea>
-                  <div class="edit-controller">
-                    <i class="fa-solid fa-check deleteBtn"></i>
-                    <i class="fa-solid fa-pen-to-square editBtn"></i>
-                  </div>
-                </div>
-                <div class="update-controller">
-                  <button class="saveBtn">Save</button>
-                  <button class="cancelBtn">Cancel</button>
-                </div>
-              </div>`
+  // Function to close task box
+  function close() {
+    modal.style.display = "none";
   }
-  document.querySelector(".to-do-list").innerHTML = items
-  activateDeleteListeners()
-  activateEditListeners()
-  activateSaveListeners()
-  activateCancelListeners()
-}
-
-function activateDeleteListeners(){
-  let deleteBtn = document.querySelectorAll(".deleteBtn")
-  deleteBtn.forEach((dB, i) => {
-    dB.addEventListener("click", () => { deleteItem(i) })
-  })
-}
-
-function activateEditListeners(){
-  const editBtn = document.querySelectorAll(".editBtn")
-  const updateController = document.querySelectorAll(".update-controller")
-  const inputs = document.querySelectorAll(".input-controller textarea")
-  editBtn.forEach((eB, i) => {
-    eB.addEventListener("click", () => { 
-      updateController[i].style.display = "block"
-      inputs[i].disabled = false })
-  })
-}
-
-function activateSaveListeners(){
-  const saveBtn = document.querySelectorAll(".saveBtn")
-  const inputs = document.querySelectorAll(".input-controller textarea")
-  saveBtn.forEach((sB, i) => {
-    sB.addEventListener("click", () => {
-      updateItem(inputs[i].value, i)
-    })
-  })
-}
-
-function activateCancelListeners(){
-  const cancelBtn = document.querySelectorAll(".cancelBtn")
-  const updateController = document.querySelectorAll(".update-controller")
-  const inputs = document.querySelectorAll(".input-controller textarea")
-  cancelBtn.forEach((cB, i) => {
-    cB.addEventListener("click", () => {
-      updateController[i].style.display = "none"
-      inputs[i].disabled = true
-      inputs[i].style.border = "none"
-    })
-  })
-}
-
-function createItem(item){
-  itemsArray.push(item.value)
-  localStorage.setItem('items', JSON.stringify(itemsArray))
-  location.reload()
-}
-
-function deleteItem(i){
-  itemsArray.splice(i,1)
-  localStorage.setItem('items', JSON.stringify(itemsArray))
-  location.reload()
-}
-
-function updateItem(text, i){
-  itemsArray[i] = text
-  localStorage.setItem('items', JSON.stringify(itemsArray))
-  location.reload()
-}
-
-window.onload = function() {
-  displayDate()
-  displayItems()
-};
