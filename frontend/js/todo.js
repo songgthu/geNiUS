c25eb97a81f847f1ba9d895a2cbfafe778ebdcbd
songgthu <e1082977@u.nus.edu>
2023-06-03 13:50:21 +0700

    Update add task backend

--- frontend/js/todo.js
+++ frontend/js/todo.js
@@ -1,3 +1,5 @@
+
+
 const create = document.querySelector('.addButton');
 var modal = document.getElementById("modal");
 create.addEventListener('click', createTask);
@@ -15,11 +17,11 @@ function createTask(){
     // Get the input value
     const taskInput = document.querySelector('.taskInput').value;
     const deadline = document.querySelector('.deadlineInput').value;
+    console.log(deadline);
     if (taskInput != "" && deadline != "") {
     var [datePart, timePart] = deadline.split('T');
 
 // Split the date component into year, month, and day
-
 var [year, month, day] = datePart.split('-');
 
 
@@ -55,6 +57,33 @@ var formattedDate = monthString + ' ' + day + ', ' + year + ', ' + timePart;
     checkboxContainer.appendChild(label);
 
     statusCell.appendChild(checkboxContainer);
+    const userId = sessionStorage.getItem("userId");
+    console.log(userId);
+    console.log(taskInput);
+    console.log(formattedDate);
+    const data = {
+      taskInput: taskInput,
+      deadline: formattedDate,
+      userId: userId
+    };
+
+    fetch(`http://localhost:5501/add-task`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(response => {
+      if (response.status === 500) {
+        alert('Internal server error');
+      } else if (response.status === 201) {
+          alert('Add task successfully');
+      } else {
+       
+      }
+    })
+    .catch(error => {
+      console.error('Error during add task:', error)});
     close();
     } else {
       alert("Please fill in all the fields");
@@ -69,3 +98,72 @@ var formattedDate = monthString + ' ' + day + ', ' + year + ', ' + timePart;
   // Discard change
   const cancel = document.querySelector(".cancelTaskButton");
   cancel.addEventListener('click', close);
+
+  // Retrieve task data
+  // Fetch task data from the database
+  document.addEventListener('DOMContentLoaded', retrieveTask);
+  const userId = sessionStorage.getItem("userId");
+  const data = { userId: userId };
+function retrieveTask(){
+  fetch(`http://localhost:5501/tasks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(response => {
+    if (response.status === 500) {
+      alert('Internal server error');
+    } else if (response.status === 201) {
+      response.json().then(data => {
+        const results = data.results; // Access the "results" property
+        sessionStorage.setItem('taskData', JSON.stringify(results));
+        console.log(sessionStorage.getItem('taskData'));
+        displayTask();
+        alert('Task retrieve successfully');
+      });
+    } else {
+     
+    }
+  })
+  .catch(error => {
+    console.error('Error during retrieve task:', error)});
+}
+
+function displayTask() {
+  //const taskData = sessionStorage.getItem('taskData');
+  const taskData = JSON.parse(sessionStorage.getItem('taskData'));
+  const table = document.querySelector(".task-table")
+  const taskBody = document.querySelector(".taskBody");
+
+  for(let i = 0; i < taskData.length; i++) {
+    const newRow = document.createElement("tr");
+    const taskCell = document.createElement("td");
+    const deadlineCell = document.createElement("td");
+    const completedCell = document.createElement("td");
+    
+    
+    deadlineCell.textContent = taskData[i].deadline;
+    taskCell.textContent = taskData[i].task;
+    
+    
+    const checkboxContainer = document.createElement("div");
+    const checkbox = document.createElement("input");
+    const checkboxLabel = document.createElement("label");
+
+    checkboxContainer.classList.add("checkbox-container");
+    checkbox.type = "checkbox";
+    checkbox.id = `statusCheckbox${completedCell.textContent}`;
+    checkboxContainer.appendChild(checkbox);
+    checkboxContainer.appendChild(checkboxLabel);
+    completedCell.appendChild(checkboxContainer);
+
+    newRow.appendChild(taskCell);
+    newRow.appendChild(deadlineCell);
+    newRow.appendChild(completedCell);
+    console.log(newRow);
+    taskBody.appendChild(newRow);
+
+  }
+
+}
