65cfdc36c67b2ce1c129ebd3d359a3115e15d7a4
songgthu <e1082977@u.nus.edu>
2023-06-10 17:30:05 +0700

    update planner feature (in progress)

--- frontend/js/update-timetable.js
+++ frontend/js/update-timetable.js
@@ -1,21 +1,23 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.flex-input');
-  form.addEventListener('submit', updateTimetable);
-  
-  // Retrieve the stored URL from sessionStorage
-  const storedUrl = sessionStorage.getItem('url');
-  if (storedUrl) {
-    const iframe = document.getElementById('timetable-iframe');
-    iframe.src = storedUrl;
-  }
-});
+// Retrieve the stored URL from sessionStorage
+const storedUrl = sessionStorage.getItem('url');
+if (storedUrl != '') {
+  const iframe = document.getElementById('timetable-iframe');
+  iframe.src = storedUrl;
+  console.log(iframe.src);
+}
+  const update = document.querySelector('.submitButton');
 
+  const form = document.querySelector('.flex-input');
+  update.addEventListener('click', updateTimetable);
+  
+  
 function updateTimetable(event) {
   event.preventDefault();
   const userEmail = sessionStorage.getItem('email');
   const input = document.querySelector('.url').value;
   const iframe = document.getElementById('timetable-iframe');
   iframe.src = input;
+  console.log(input);
 
   const data = {
     url: input,
@@ -39,3 +41,57 @@ function updateTimetable(event) {
     }
   });
 }
+
+
+const apiUrl = 'https://api.nusmods.com/v2/';
+fetch(`http://localhost:5501/modules`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
+    .then(data => {
+      // Process the retrieved data
+      
+      const targetLabSession = "16A";
+      const targetRecitationSession = "13";
+      
+      const semesterData = data.semesterData;
+      for (let i = 0; i < semesterData.length; i++) {
+        const timetable = semesterData[i].timetable;
+        for (let j = 0; j < timetable.length; j++) {
+          const lessonType = timetable[j].lessonType;
+          const classNo = timetable[j].classNo;
+      
+          if (lessonType === "Laboratory" && classNo === targetLabSession) {
+            const startTime = timetable[j].startTime;
+            const endTime = timetable[j].endTime;
+            const venue = timetable[j].venue;
+            const day = timetable[j].day;
+      
+            console.log(`Lab Session - Class ${classNo}`);
+            console.log(`Time: ${startTime} - ${endTime}`);
+            console.log(`Venue: ${venue}`);
+            console.log(`Day: ${day}`);
+            console.log("--------------------");
+          } else if (lessonType === "Recitation" && classNo === targetRecitationSession) {
+            const startTime = timetable[j].startTime;
+            const endTime = timetable[j].endTime;
+            const venue = timetable[j].venue;
+            const day = timetable[j].day;
+      
+            console.log(`Recitation Session - Class ${classNo}`);
+            console.log(`Time: ${startTime} - ${endTime}`);
+            console.log(`Venue: ${venue}`);
+            console.log(`Day: ${day}`);
+            console.log("--------------------");
+          }
+        }
+      }
+      
+      
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error('Error:', error);
+    });
