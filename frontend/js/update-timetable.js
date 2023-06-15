2dbad300232c64f6bc8d6de558f918cbbbf77136
songgthu <e1082977@u.nus.edu>
2023-06-15 19:56:18 +0700

    fix timetable backend

--- frontend/js/update-timetable.js
+++ frontend/js/update-timetable.js
@@ -1,21 +1,16 @@
 // Retrieve the stored URL from sessionStorage
-const storedUrl = sessionStorage.getItem('url');
-if (storedUrl != '') {
-  const iframe = document.getElementById('timetable-iframe');
-  iframe.src = storedUrl;
-  console.log(iframe.src);
-}
-  const update = document.querySelector('.submitButton');
+const iframe = document.getElementById('timetable-iframe');
+const userEmail = sessionStorage.getItem('email');
+const update = document.querySelector('.submitButton');
 
-  const form = document.querySelector('.flex-input');
-  update.addEventListener('click', updateTimetable);
+const form = document.querySelector('.flex-input');
+update.addEventListener('click', updateTimetable);
   
   
 function updateTimetable(event) {
   event.preventDefault();
-  const userEmail = sessionStorage.getItem('email');
+  
   const input = document.querySelector('.url').value;
-  const iframe = document.getElementById('timetable-iframe');
   iframe.src = input;
   console.log(input);
 
@@ -40,6 +35,30 @@ function updateTimetable(event) {
       // Handle other responses if needed
     }
   });
+}
+
+document.addEventListener('DOMContentLoaded', retrieveTimetable)
+function retrieveTimetable() {
+  const data = { userEmail: userEmail };
+  fetch(`http://localhost:5501/get-timetable`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(response => {
+    if (response.status === 500) {
+      alert('Internal server error');
+    } else if (response.status === 201) {
+      response.json().then(data => {
+        iframe.src = data.results[0].url; 
+        alert('Get timetable successful');
+      })
+      
+      
+    } 
+  });
+  
 }
 
 
