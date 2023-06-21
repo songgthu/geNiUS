2a26ccd7f1e383372cc0938d4d60c9877e6c3814
songgthu <e1082977@u.nus.edu>
2023-06-21 13:27:38 +0700

    exam-overview update backend (in progress)

--- frontend/js/exam-overview.js
+++ frontend/js/exam-overview.js
@@ -1,7 +1,8 @@
-
+var currentURL = window.location.href.split("/")[2];
+var examObjects = {};
 const modal = document.querySelector(".exam-modal");
 const editModal = document.querySelector(".edit-exam-modal");
-const overlay = document.querySelector(".overlay");
+
 const openModalBtn = document.querySelector(".create-exam-button");
 const closeModalBtn = document.querySelector(".close-exam-modal");
 const closeEditModalBtn = document.querySelector(".close-edit-exam-modal");
@@ -29,6 +30,8 @@ function closeEditModal() {
 
 const addContentBtn = document.querySelector('.add-content-button');
 addContentBtn.addEventListener('click', addContent);
+const addEditContentBtn = document.querySelector('.add-edit-content-button');
+addEditContentBtn.addEventListener('click', addEditContent);
 
 function addContent() {
   const content = modal.querySelector('.content-input').value;
@@ -46,6 +49,24 @@ function addContent() {
     attachEditContentListener();
   }
 }
+
+function addEditContent() {
+  const content = editModal.querySelector('.edit-content-input').value;
+  if (content == '') {
+    alert('Cannot add empty content');
+  } else {
+    const contentList = editModal.querySelector('.edit-review-content-list');
+    contentList.innerHTML += `<li class="content-item">
+    
+    <span class="content-text">${content}</span>
+    <span class="material-symbols-outlined editContent"> edit </span>
+    <span class="material-symbols-outlined deleteContent"> delete </span>
+    </li>`;
+    attachRemoveContentListener();
+    attachEditContentListener();
+  }
+}
+
 function attachRemoveContentListener() {
   modal.querySelectorAll('.material-symbols-outlined.deleteContent').forEach((icon) => {
     icon.addEventListener('click', removeContent);
@@ -89,22 +110,29 @@ submitExam.addEventListener('click', addExam);
 var countdownArray = [];
 
 function addExam() {
+  // Select input values from modal
   const name = modal.querySelector('.examNameInput').value || null;
   const date = modal.querySelector('.examDateInput').value || null;
   const venue = modal.querySelector('.examVenueInput').value || null;
 
   var contentArr = [];
   modal.querySelector('.review-content-list').querySelectorAll('.content-text').
-  forEach((content) => contentArr.push(content.textContent));
+  forEach((content) => contentArr.push([content.textContent, 'unchecked']));
+  // Create to-do list
   const newList = document.createElement('ul');
   newList.classList.add(`review-to-do-list-${name}`);
 
 // Create list items and append them to the new <ul> element
 for (let i = 0; i <= contentArr.length - 1; i++) {
   const listItem = document.createElement('li');
-  listItem.textContent = contentArr[i];
-  listItem.innerHTML += `<input type="checkbox" class="content-checkbox"></input>`;
+  listItem.textContent = contentArr[i][0];
+  // const checkbox = document.createElement('input');
+  // checkbox.type = 'checkbox';
+  // checkbox.classList.add('content-checkbox', `${contentArr[i][1]}`);
+  listItem.innerHTML += `<input type="checkbox" class="content-checkbox ${contentArr[i][1]}"></input>`;
   newList.appendChild(listItem);
+
+  
 }
 
   if (name == null || date == null || venue == null) {
@@ -156,27 +184,92 @@ for (let i = 0; i <= contentArr.length - 1; i++) {
     </div>
 
     </div>`;
+
+    examObjects[name] = [date, venue, contentArr];
     
-    const examArray = [name, date, venue, modal.querySelector('.review-content-list').innerHTML];
     
+    
+    // Add event listener to show/hide review content
     attachContentEventListener();
+
+    // Add event listener to check/uncheck checkbox
+    attachCheckboxEventListener(name);
+
+    // Add event listener to remove exam
     attachRemoveExamEventListener();
-    attachEditExamEventListener(examArray);
-    countdownArray.push([date, name]);
-    attachCountdownFunction(countdownArray);
     closeModal();
+
+    // Add event listener to start countdown
+    attachCountdownFunction();
+
+    attachEditExamEventListener(name);
+
+    const exam = {
+      name: name,
+      date: date,
+      venue: venue,
+      todolist: contentArr,
+      userId: sessionStorage.getItem(userId)
+    };
+    fetch(`https://${currentURL}/add-exam`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(exam)
+  }).then(response => {
+    if (response.status === 500) {
+      alert('Internal server error');
+    } else if (response.status === 409) {
+      alert('Exam name already exist')
+    } else if (response.status === 201) {
+      alert('Add exam successfully');
+    }
+  }).catch(error => {
+      console.error('Error during query:', error)});
+
   }
 }
 
-function attachCountdownFunction(arr) {
+function attachCheckboxEventListener(name) {
+  document.querySelectorAll('.content-checkbox').forEach((checkbox) => checkbox.addEventListener('change', function () {
+    if(checkbox.checked) {
+      checkbox.classList.remove('unchecked');
+      checkbox.classList.add('checked');
+      
+      for (var content in examObjects[name][2]) {
+        if(checkbox.parentElement.textContent === examObjects[name][2][content][0]) {
+          examObjects[name][2][content][1] = 'checked';
+          console.log(examObjects[name][2]);
+        }
+      }
+      
+      console.log('checked');
+    } else {
+      checkbox.classList.remove('checked');
+      checkbox.classList.add('unchecked');
+
+      for (var content in examObjects[name][2]) {
+        if(checkbox.parentElement.textContent === examObjects[name][2][content][0]) {
+          examObjects[name][2][content][1] = 'unchecked';
+          console.log(examObjects[name][2]);
+        }
+      }
+
+      console.log('unchecked');
+    }
+  }));
+}
+
+function attachCountdownFunction() {
   function formatTime(time) {
     return time.toString().padStart(2, '0');
   }
-  console.log(arr);
-  for (let i = 0; i < arr.length; i++) {
-    const countdownDate = new Date(arr[i][0]).getTime();
+
+  for (var name in examObjects) {
+    const countdownDate = new Date(examObjects[name][0]).getTime();
     // select elements to input values
-    const countdown = document.querySelector(`.exam-countdown-${arr[i][1]}`);
+    const countdown = document.querySelector(`.exam-countdown-${name}`);
     const dayHolder = countdown.querySelector("#countdown-days");
     const hourHolder = countdown.querySelector("#countdown-hours");
     const minuteHolder = countdown.querySelector("#countdown-minutes");
@@ -230,6 +323,7 @@ function attachCountdownFunction(arr) {
   }
   
   function stopCountdown() {
+    delete examObjects.name;
     clearInterval(countdownInterval);
     dayHolder.textContent = '00';
     hourHolder.textContent = '00';
@@ -275,28 +369,148 @@ attachRemoveExamEventListener();
 
 function removeExam(name) {
   const exam = document.querySelector(`.exam-container-${name}`);
-  console.log(exam);
+  const data = { 
+    name: name, 
+    userId: sessionStorage.getItem('userId')
+  };
+  fetch(`https://${currentURL}/delete-exam`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(response => {
+    if (response.status === 500) {
+      alert('Internal server error');
+    } else if (response.status === 409) {
+      alert('Error during delete exam')
+    } else if (response.status === 201) {
+      alert('Delete exam successfully');
+    }
+  }).catch(error => {
+      console.error('Error during query:', error)});
+  delete examObjects[name];
+  console.log(examObjects);
+  console.log(examObjects.name);
   exam.remove();
 }
 
-function attachEditExamEventListener(arr) {
+function attachEditExamEventListener(name) {
   const editExamBtns = document.querySelectorAll(`[class^="material-symbols-outlined edit-exam-"]`);
   editExamBtns.forEach((btn) => btn.addEventListener('click', function() {
-    editExam(arr[0], arr[1], arr[2], arr[3]);
+    editExam(name);
   }));
 }
 
 
-function editExam(oldName, oldDate, oldVenue, oldContentList) {
+function editExam(oldName) {
   openEditModal();
   editModal.querySelector('.edit-examNameInput').value = oldName;
-  editModal.querySelector('.edit-examDateInput').value = oldDate;
-  editModal.querySelector('.edit-examVenueInput').value = oldVenue;
-  editModal.querySelector('.edit-review-content-list').innerHTML = oldContentList;
+  editModal.querySelector('.edit-examDateInput').value =  examObjects[oldName][0];
+  editModal.querySelector('.edit-examVenueInput').value = examObjects[oldName][1];
+  editModal.querySelector('.edit-review-content-list').innerHTML = '';
+  for (let i = 0; i < examObjects[oldName][2].length; i++) {
+    editModal.querySelector('.edit-review-content-list').innerHTML += `<li class="content-item">
+    
+    <span class="content-text">${examObjects[oldName][2][i][0]}</span>
+    <span class="material-symbols-outlined editContent"> edit </span>
+    <span class="material-symbols-outlined deleteContent"> delete </span>
+    </li>`;
+  }
+  
+
   attachEditContentListener();
   attachRemoveContentListener();
 
   const updateExamBtn = editModal.querySelector('.update-exam-button');
+  updateExamBtn.addEventListener('click', function() {
+    updateExam(oldName); }
+    );
 
 }
 
+
+function updateExam(oldName) {
+  const newName = editModal.querySelector('.edit-examNameInput').value;
+  const newDate = editModal.querySelector('.edit-examDateInput').value;
+  const newVenue = editModal.querySelector('.edit-examVenueInput').value;
+ 
+
+  var contentArr = [];
+  editModal.querySelector('.edit-review-content-list').querySelectorAll('.content-text').
+  forEach((content) => contentArr.push([content.textContent, 'unchecked']));
+  // Create to-do list
+  const newList = document.createElement('ul');
+  newList.classList.add(`review-to-do-list-${newName}`);
+
+// Create list items and append them to the new <ul> element
+for (let i = 0; i <= contentArr.length - 1; i++) {
+  const listItem = document.createElement('li');
+  listItem.textContent = contentArr[i][0];
+  //listItem.innerHTML += `<input type="checkbox" class="content-checkbox "></input>`;
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.classList.add('content-checkbox', `${contentArr[i][1]}`);
+  listItem.appendChild(checkbox);
+  newList.appendChild(listItem);
+}
+
+  const container = document.querySelector(`.exam-container-${oldName}`);
+  container.classList.replace(`exam-container-${oldName}`, `exam-container-${newName}`);
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  };
+  const newFormattedDate = new Date(newDate).toLocaleDateString('en-US', options);
+
+    document.querySelector(`.exam-container-${newName}`).innerHTML = `
+    <span class="material-symbols-outlined edit-exam-${newName}"> edit </span>
+    <span class="material-symbols-outlined remove-exam-${newName}">remove</span>
+    <h3 class="exam-title"> ${newName.toUpperCase()} </h3>
+    <p class="exam-info"> 
+    Date: ${newFormattedDate} <br>
+    Venue: ${newVenue} <br>
+    </p> 
+    <div class="exam-countdown-${newName}">
+    <div class="timing-part">
+      <div class="countdown-item">
+        <span class="countdown-number" id="countdown-days">00</span>
+        <span class="countdown-tag">DAYS</span>
+      </div>
+      <div class="countdown-item">
+        <span class="countdown-number" id="countdown-hours">00</span>
+        <span class="countdown-tag">HOURS</span>
+      </div>
+      <div class="countdown-item">
+        <span class="countdown-number" id="countdown-minutes">00</span>
+        <span class="countdown-tag">MINUTES</span>
+      </div>
+      <div class="countdown-item">
+        <span class="countdown-number" id="countdown-seconds">00</span>
+        <span class="countdown-tag">SECONDS</span>
+      </div>
+    </div>
+    </div>
+    <button class="toggle-content-${newName}">Show Review Content</button>
+
+    <div class="review-content-${newName}" style="display:none"> Review To-do List:
+    ${newList.innerHTML}
+    </div>`;
+
+  delete examObjects[oldName];
+    
+    examObjects[newName] = [newDate, newVenue, contentArr];
+    console.log(examObjects);
+
+    attachContentEventListener();
+    attachCheckboxEventListener(newName);
+    attachRemoveExamEventListener();
+    attachEditExamEventListener(newName);
+    closeEditModal();
+    attachCountdownFunction();
+
+    
+}
